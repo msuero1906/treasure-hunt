62c6b715b806dff416fecc0a3e6468f79edfcc45
msuero1906 <43377325+msuero1906@users.noreply.github.com>
2019-01-17 08:47:15 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,21 @@
+$(".num1").click(function(){
+    $("#num1").show();
+
+});
+$(".num2").click(function(){
+    $("#num2").fadeIn();
+
+});
+$(".num3").click(function(){
+    $("#num3").show();
+
+});
+$(".num4").click(function(){
+    $("#num4").fadeIn();
+
+});
+$("h1").click(function(){
+    $("h2").show();
+
+});
+
